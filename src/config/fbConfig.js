1ea5684f5c4b5ef013f62b63b4f7644adc78a8da
Orion55 <grol55@mail.ru>
2020-03-11 23:50:46 +0500

    Установка библиотек

---
+++ src/config/fbConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAEOYkMM2xYokEEFTHRmok_l1acX4_Pnn0',
+  authDomain: 'mymoney-94ccc.firebaseapp.com',
+  databaseURL: 'https://mymoney-94ccc.firebaseio.com',
+  projectId: 'mymoney-94ccc',
+  storageBucket: 'mymoney-94ccc.appspot.com',
+  messagingSenderId: '129188174683',
+  appId: '1:129188174683:web:f9211c3e2b6ab8feff987e',
+};
